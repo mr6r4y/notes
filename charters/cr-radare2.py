14c2baca15ab72bcdd0f4dff063ba5909f332f3c
mr6r4y <mr6r4y@gmail.com>
2017-10-17 17:26:12 +0300

    Add

--- charters/cr-radare2.py
+++ charters/cr-radare2.py
@@ -11,9 +11,8 @@ def main():
     d_structs = gv.Digraph(name="cluster_structs")
     d_structs.attr(label="structs")
     d_structs.node_attr["shape"] = "record"
-    # d_structs.graph_attr["splines"] = "ortho"
-    # d_structs.attr(rankdir="LR")
 
+    d_structs.node("v1", label=plg.struct_to_label(RVector))
     d_structs.node("h1", label=plg.struct_to_label(RBinHeap))
     d_structs.node("h2", label=plg.struct_to_label(RBinHeap))
 
@@ -21,12 +20,35 @@ def main():
 
     d_funcs = gv.Digraph(name="cluster_funcs")
     d_funcs.attr(label="Function CFG")
+    # graph_attr["splines"] = "ortho"
+
+    d_funcs.node(name="R_NEW", label="#define R_NEW(x) (x*)malloc(sizeof(x))")
 
     d_funcs.edges([["r_binheap_clear", "r_vector_clear"]])
     d_funcs.edges([["r_binheap_init", "r_vector_init"]])
-    d_funcs.edges([["r_binheap_new", "R_NEW"], ["R_NEW", "malloc(sizeof(..))"]])
+    d_funcs.edges([["r_binheap_new", "R_NEW"]])
     d_funcs.edges([["r_binheap_pop", "_heap_down"]])
     d_funcs.edges([["r_binheap_push", "r_vector_push"], ["r_vector_push", "_heap_up"]])
+
+    plg.nodes(d_funcs, [
+        ("R_NEW0", "#define R_NEW0(x) (x*)calloc(1,sizeof(x))"),
+        ("RESIZE_OR_RETURN_NULL", '''#define RESIZE_OR_RETURN_NULL(next_capacity) do { \\
+        int new_capacity = next_capacity; \\
+        void **new_a = realloc (vec->a, sizeof(void *) * new_capacity); \\
+        if (!new_a) { \\
+            return NULL; \\
+        } \\
+        vec->a = new_a; \\
+        vec->capacity = new_capacity; \\
+    } while (0)'''),
+        ("NEXT_VECTOR_CAPACITY", """#define NEXT_VECTOR_CAPACITY (vec->capacity < INITIAL_VECTOR_LEN \\
+    ? INITIAL_VECTOR_LEN \\
+    : vec->capacity <= 12 ? vec->capacity * 2 \\
+    : vec->capacity + (vec->capacity >> 1))"""),
+        ("R_FREE", "#define R_FREE(x) { free(x); x = NULL; }"),
+    ])
+    d_funcs.edge("r_vector_clear", "R_FREE")
+    plg.arrow(d_funcs, ["r_vector_clone", ["R_NEW", "malloc", "R_FREE", "memcpy"]])
 
     d_includes = gv.Digraph(name="cluster_includes")
     d_includes.attr(label="Includes")
@@ -35,8 +57,11 @@ def main():
     plg.arrow(d_includes, ["r_types.h", ["r_userconf.h", "<r_endian.h>", "r_util/r_str_util.h", "<sys/param.h>",
                                          "<winsock2.h>", "<windows.h>", "<stdio.h>", "<string.h>", "<stdlib.h>",
                                          "<stdarg.h>", "<fcntl.h>"]])
+    plg.arrow(d_includes, ["r_vector.c", "r_vector.h"])
 
     d = gv.Digraph()
+    # d.graph_attr["splines"] = "ortho"
+    d.node_attr["shape"] = "box"
     d.subgraph(d_structs)
     d.subgraph(d_funcs)
     d.subgraph(d_includes)
